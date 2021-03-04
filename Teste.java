540fb8b3a17885d35749dcbb98da45a01b8eb942
medynahs <61702013+medynahs@users.noreply.github.com>
2021-03-04 16:39:35 -0300

    Exercício 2

--- Teste.java
+++ Teste.java
@@ -1,14 +1,21 @@
-
-public class Teste {
+public class Teste { 
     public static void main(String[] args) {
-        double peso;
-        double valor;
-        int distancia;
 
-       Scanner sc = new Scanner(System.in);
+    Imposto cofins = new Cofins(10000);
+    System.out.println("O custo do imposto Cofins custará:");
+    System.out.println(cofins.calculaImposto());
+
+    Imposto icms = new Icms(30000);
+    System.out.println("O custo do imposto Icms custará:");
+    System.out.println(icms.calculaImposto());
+
+    Imposto ipi = new Ipi(20000);
+    System.out.println("O custo do imposto Ipi custará:");
+    System.out.println(ipi.calculaImposto());
 
 
-        
-    }
+    System.out.println("A somatória dos impostos é: "+ (cofins.calculaImposto() + icms.calculaImposto() + ipi.calculaImposto()));
+
+ }
     
 }
