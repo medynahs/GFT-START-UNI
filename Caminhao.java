4fa11cd93e48fd11003d1c8266af80f130929df9
medynahs <61702013+medynahs@users.noreply.github.com>
2021-03-04 16:33:54 -0300

    Exercício 4

---
+++ Caminhao.java
@@ -0,0 +1,25 @@
+public class Caminhao extends Transporte{
+    private double valor;
+    private double peso;
+
+    public Caminhao(){
+
+    }
+
+    public double getValor(){
+        return valor;
+    }
+
+    public void setValor(double valor){
+        this.valor = valor;
+    }
+
+    public double getPeso(){
+        return peso;
+    }
+
+    public void setPeso(double peso){
+        this.peso = peso;
+    }
+    
+}
