182753997909101f6714bf8e8024c4f3800fcb42
medynahs <61702013+medynahs@users.noreply.github.com>
2021-03-04 15:37:58 -0300

    Exercício 4

---
+++ ex4.py
@@ -0,0 +1,15 @@
+qtd=int(input("Digite um valor inteiro:"));
+
+# criei a variável quantidade (qtd) e li seu valor.
+
+i = str
+i = "incomoda "
+
+# criei variável i para representar o texto "incomoda" que se repete na música
+
+print(qtd*i, "muito mais");
+
+# imprimi a quantidade de vezes que o i se repetirá vezes o i e adicionei o resto do texto.
+
+
+
