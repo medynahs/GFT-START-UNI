383dd7f59ac11645dd56bb60077ec669aebd924d
medynahs <61702013+medynahs@users.noreply.github.com>
2021-03-04 16:41:16 -0300

    Exercício 3

---
+++ Teste Ex.3.java
@@ -0,0 +1,14 @@
+
+public class Teste {
+    public static void main(String[] args) {
+        double peso;
+        double valor;
+        int distancia;
+
+       Scanner sc = new Scanner(System.in);
+
+
+        
+    }
+    
+}
